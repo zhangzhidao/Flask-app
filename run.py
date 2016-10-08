64e9715175809ec107cf01afe4df9c3b7a2c896d
zhangzhidao <850288883@qq.com>
2016-10-08 23:09:46 +0800

    3-1 jinjia2模板+bootstrap继承

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return 'hello %s!' % name
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
