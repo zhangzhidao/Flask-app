804bb9bbc14858409de89fda0f5aa758f298647d
zhangzhidao <850288883@qq.com>
2016-10-08 23:46:37 +0800

    3-3 基模板中定义favicon.ico 图标

--- run.py
+++ run.py
@@ -1,17 +1,31 @@
+# coding=utf-8
 from flask import Flask, render_template
+# 引入Bootstrap模板
 from flask.ext.bootstrap import Bootstrap
 
+# 初始化Flask
 app = Flask(__name__)
+# 初始化Bootstrap
 bootstrap = Bootstrap(app)
 
+
+# index路由
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# user路由
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# 启动
 if __name__ == '__main__':
     app.run(debug=True, port=80)
